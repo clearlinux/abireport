e4fc19be1218de18070746458bf322177304859e
Ikey Doherty <michael.i.doherty@intel.com>
2017-02-03 16:23:29 +0000

    Bump version to 1.0.3
    Signed-off-by: Ikey Doherty <michael.i.doherty@intel.com>

--- src/abireport/cmd/version.go
+++ src/abireport/cmd/version.go
@@ -23,7 +23,7 @@ import (
 
 const (
 	// ABIReportVersion is the public version of the tool
-	ABIReportVersion = "1.0.2"
+	ABIReportVersion = "1.0.3"
 )
 
 // versionCmd handles "abireport version"
