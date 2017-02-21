aca974ceece09412c17ab04ddab442bbfd863bb5
Ikey Doherty <michael.i.doherty@intel.com>
2017-02-21 17:39:56 +0000

    Bump version 1.0.5
    Signed-off-by: Ikey Doherty <michael.i.doherty@intel.com>

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 PROJECT_ROOT := src/
-VERSION = 1.0.4
+VERSION = 1.0.5
 
 .DEFAULT_GOAL := all
 
--- src/abireport/cmd/version.go
+++ src/abireport/cmd/version.go
@@ -23,7 +23,7 @@ import (
 
 const (
 	// ABIReportVersion is the public version of the tool
-	ABIReportVersion = "1.0.4"
+	ABIReportVersion = "1.0.5"
 )
 
 // versionCmd handles "abireport version"
