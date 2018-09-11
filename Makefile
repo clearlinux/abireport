fac866c83a5686fd7b7fc56769c74d30493dd43d
Patrick McCarty <patrick.mccarty@intel.com>
2018-09-10 18:27:40 -0700

    Release v1.0.6
    Makes eopkg extraction more portable by using unzip instead of uneopkg.
    
    Signed-off-by: Patrick McCarty <patrick.mccarty@intel.com>

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 PROJECT_ROOT := src/
-VERSION = 1.0.5
+VERSION = 1.0.6
 
 .DEFAULT_GOAL := all
 
--- src/abireport/cmd/version.go
+++ src/abireport/cmd/version.go
@@ -23,7 +23,7 @@ import (
 
 const (
 	// ABIReportVersion is the public version of the tool
-	ABIReportVersion = "1.0.5"
+	ABIReportVersion = "1.0.6"
 )
 
 // versionCmd handles "abireport version"
